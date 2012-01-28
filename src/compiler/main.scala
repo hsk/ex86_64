152e3a4f19e1028f4d4a8d4ca67d8b41c92a6df0
Hiroshi Sakurai <sakurai777@gmail.com>
2012-01-29 01:34:42 +0900

    add main

---
+++ src/compiler/main.scala
@@ -0,0 +1,18 @@
+package compiler
+
+import java.io._
+import reader._
+
+object main {
+  def main(argv:Array[String]) {
+    val prg = exec.readAll(new FileInputStream(argv(0)))
+    val st = parse(prg)
+    val ast = st2ast(st)
+    val s = setmem(ast)
+    val e = expand(s)
+    val m = memAlloc(e)
+    emit("e.s", m)
+    exec("gcc -m64 -o e e.s lib.c")
+    exec("./e")
+  }
+}
